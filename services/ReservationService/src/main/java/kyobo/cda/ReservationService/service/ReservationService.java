577f572be82fb1d463ada6459329d50bf78b5889
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-11-06 17:49:36 +0900

    Feat: Reservation 예약 가능 자리 증감 및 알림 요청 처리

--- services/ReservationService/src/main/java/kyobo/cda/ReservationService/service/ReservationService.java
+++ services/ReservationService/src/main/java/kyobo/cda/ReservationService/service/ReservationService.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -41,6 +42,15 @@ public class ReservationService {
                         request.getRestaurantId(), request.getReservationDate(), request.getReservationTime())
                 .orElseThrow(() -> new IllegalArgumentException("해당 시간에 예약이 불가능합니다."));
 
+        // 예약 가능한 테이블이 있는지 확인
+        if (availability.getAvailableTables() <= 0) {
+            throw new IllegalStateException("예약 가능한 자리가 없습니다.");
+        }
+
+        // 예약 후 availableTables 감소
+        availability.setAvailableTables(availability.getAvailableTables() - 1);
+        restaurantAvailabilityRepository.save(availability);
+
         // 예약 생성
         Reservation reservation = Reservation.builder()
                 .restaurantId(request.getRestaurantId())
@@ -52,20 +62,8 @@ public class ReservationService {
 
         reservationRepository.save(reservation);
 
-        // 예약 생성 시 Notification 서버로 예약 정보 전송
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<ReservationNotiDto> entity = new HttpEntity<>(ReservationNotiDto.builder()
-                .reservationId(reservation.getId())
-                .restaurantName(availability.getRestaurantName())
-                .userEmail(reservation.getUserEmail())
-                .reservationDateTime(reservation.getReservationDateTime())
-                .numberOfGuests(reservation.getNumberOfGuests())
-                .build(), headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(notificationServerUrl+"/notification/confirm", HttpMethod.POST, entity, String.class);
-        log.info("Notification 서버로 예약 정보 전송 완료. 응답: {}", response.getBody());
+        ResponseEntity<String> response = sendNotification(reservation, availability, "/notification/confirm");
+        log.info("Notification 서버로 예약 생성 요청 완료, 응답: {}", response.getBody());
 
         return ReservationDto.builder()
                 .reservationId(reservation.getId())
@@ -78,32 +76,57 @@ public class ReservationService {
 
     // 예약 취소 메서드
     @Transactional
-    public void cancelReservation(UUID reservationId) {
+    public void cancelReservation(ReservationRequestDto request, UUID reservationId) {
         Reservation reservation = reservationRepository.findById(reservationId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 예약을 찾을 수 없습니다."));
+
+        RestaurantAvailability availability = restaurantAvailabilityRepository.findByRestaurantIdAndReservationDateAndReservationTime(
+                        request.getRestaurantId(), request.getReservationDate(), request.getReservationTime())
+                .map(restaurantAvailability -> {
+                    log.info("예약 취소: {}", reservation);
+                    restaurantAvailability.setAvailableTables(restaurantAvailability.getAvailableTables() + 1);
+                    return restaurantAvailabilityRepository.save(restaurantAvailability);
+                }).orElseThrow(() -> new IllegalArgumentException("해당 예약 시간을 찾을 수 없습니다."));
 
         // 예약 삭제
         reservationRepository.deleteById(reservationId);
         log.info("예약이 삭제되었습니다. 예약 ID: {}", reservationId);
 
-        // 예약 생성 시 Notification 서버로 예약 정보 전송
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        ResponseEntity<String> response = sendNotification(reservation, availability, "/notification/cancel");
+        log.info("Notification 서버로 예약 취소 요청 완료, 응답: {}", response.getBody());
 
-        HttpEntity<ReservationNotiDto> entity = new HttpEntity<>(ReservationNotiDto.builder()
-                .reservationId(reservation.getId())
-                .userEmail(reservation.getUserEmail())
-                .reservationDateTime(reservation.getReservationDateTime())
-                .numberOfGuests(reservation.getNumberOfGuests())
-                .build(), headers);
+        // 해당 예약 시간의 대기 목록 조회
+        List<WaitListDto> waitingInfoList = new ArrayList<>();
+        List<WaitList> waitListEntries = waitListRepository.findByAvailability(availability);
+        if (!waitListEntries.isEmpty()) {
+            log.info("대기 중인 사용자 목록 조회 완료: 총 {}명", waitListEntries.size());
+            for (WaitList waitList : waitListEntries) {
+                log.info("대기 사용자 이메일: {}", waitList.getUserEmail());
+                waitingInfoList.add(WaitListDto.builder()
+                        .waitListId(waitList.getId())
+                        .restaurantId(waitList.getRestaurantId())
+                        .restaurantName(availability.getRestaurantName())
+                        .userEmail(waitList.getUserEmail())
+                        .reservationDateTime(reservation.getReservationDateTime())
+                        .build());
+            }
 
-        ResponseEntity<String> response = restTemplate.exchange(notificationServerUrl+"/notification/cancel", HttpMethod.POST, entity, String.class);
-        log.info("Notification 서버로 예약 취소 정보 전송 완료. 응답: {}", response.getBody());
+            ResponseEntity<String> waitingNotificationResponse = sendWaitingNotification(waitingInfoList);
+
+            waitListRepository.deleteAll(waitListEntries);
+        } else {
+            log.info("해당 시간에 대기 중인 사용자가 없습니다.");
+        }
+        log.info("예약 취소 완료: {}", reservationId);
     }
 
     @Transactional
     public List<ReservationDto> getReservationsByEmail(String email) {
         List<Reservation> reservations = reservationRepository.findByUserEmail(email);
+
+        if (reservations.isEmpty()) {
+            throw new IllegalArgumentException("예약 내역이 존재하지 않습니다.");
+        }
 
         return reservations.stream()
                 .map(reservation -> {
@@ -142,12 +165,36 @@ public class ReservationService {
             return WaitListDto.builder()
                     .waitListId(waitList.getId())
                     .restaurantId(waitList.getRestaurantId())
+                    .restaurantName(availability.getRestaurantName())
                     .userEmail(waitList.getUserEmail())
                     .reservationDateTime(LocalDateTime.of(request.getReservationDate(), request.getReservationTime()))
-                    .numberOfGuests(waitList.getNumberOfGuests())
                     .build();
         } else {
             throw new IllegalStateException("예약 가능한 테이블이 있어 대기 등록이 불가능합니다.");
         }
+    }
+
+    private ResponseEntity<String> sendNotification(Reservation reservation, RestaurantAvailability availability, String path) {
+        // 예약 생성 시 Notification 서버로 예약 정보 전송
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<ReservationNotiDto> entity = new HttpEntity<>(ReservationNotiDto.builder()
+                .reservationId(reservation.getId())
+                .restaurantName(availability.getRestaurantName())
+                .userEmail(reservation.getUserEmail())
+                .reservationDateTime(reservation.getReservationDateTime())
+                .numberOfGuests(reservation.getNumberOfGuests())
+                .build(), headers);
+        return restTemplate.exchange(notificationServerUrl+path, HttpMethod.POST, entity, String.class);
+    }
+
+    private ResponseEntity<String> sendWaitingNotification(List<WaitListDto> waitingInfoList) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<List<WaitListDto>> entity = new HttpEntity<>(waitingInfoList, headers);
+        return restTemplate.exchange(notificationServerUrl+"/notification/waiting", HttpMethod.POST, entity, String.class);
     }
 }
