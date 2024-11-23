224e82bef3490dceda6091e488c585a9f03385a2
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-11-23 22:56:51 +0900

    Update: Aws signer 추가

--- services/SearchService/src/main/java/kyobo/cda/SearchService/config/AWSRequestSigningApacheInterceptor.java
+++ services/SearchService/src/main/java/kyobo/cda/SearchService/config/AWSRequestSigningApacheInterceptor.java
@@ -1,181 +1,185 @@
+/*
+ * Copyright 2012-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package kyobo.cda.SearchService.config;
 
-import com.amazonaws.ReadLimitInfo;
-import com.amazonaws.SignableRequest;
-import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.DefaultRequest;
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.Signer;
 import com.amazonaws.http.HttpMethodName;
-import org.apache.http.*;
-import org.apache.http.client.methods.HttpRequestWrapper;
-import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.Header;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.HttpCoreContext;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
+import static org.apache.http.protocol.HttpCoreContext.HTTP_TARGET_HOST;
+
+/**
+ * An {@link HttpRequestInterceptor} that signs requests using any AWS {@link Signer}
+ * and {@link AWSCredentialsProvider}.
+ */
 public class AWSRequestSigningApacheInterceptor implements HttpRequestInterceptor {
-    private final AWS4Signer signer;
+    /**
+     * The service that we're connecting to. Technically not necessary.
+     * Could be used by a future Signer, though.
+     */
+    private final String service;
+
+    /**
+     * The particular signer implementation.
+     */
+    private final Signer signer;
+
+    /**
+     * The source of AWS credentials for signing.
+     */
     private final AWSCredentialsProvider awsCredentialsProvider;
 
-    public AWSRequestSigningApacheInterceptor(String serviceName, String region, AWSCredentialsProvider awsCredentialsProvider) {
-        this.signer = new AWS4Signer();
-        this.signer.setServiceName(serviceName);
-        this.signer.setRegionName(region);
+    /**
+     *
+     * @param service service that we're connecting to
+     * @param signer particular signer implementation
+     * @param awsCredentialsProvider source of AWS credentials for signing
+     */
+    public AWSRequestSigningApacheInterceptor(final String service,
+                                              final Signer signer,
+                                              final AWSCredentialsProvider awsCredentialsProvider) {
+        this.service = service;
+        this.signer = signer;
         this.awsCredentialsProvider = awsCredentialsProvider;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
-        // 요청을 래핑합니다.
-        HttpRequestWrapper requestWrapper = HttpRequestWrapper.wrap(request);
-
-        // 컨텍스트에서 타겟 호스트를 가져옵니다.
-        HttpHost targetHost = (HttpHost) context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
-        if (targetHost == null) {
-            throw new HttpException("Target host not set in the context");
+    public void process(final HttpRequest request, final HttpContext context)
+            throws HttpException, IOException {
+        URIBuilder uriBuilder;
+        try {
+            uriBuilder = new URIBuilder(request.getRequestLine().getUri());
+        } catch (URISyntaxException e) {
+            throw new IOException("Invalid URI" , e);
         }
 
-        // 요청 URI를 가져와서 전체 URI를 구성합니다.
-        String uri = requestWrapper.getURI().toString();
-        if (!uri.startsWith("http://") && !uri.startsWith("https://")) {
-            uri = targetHost.toURI() + uri;
+        // Copy Apache HttpRequest to AWS DefaultRequest
+        DefaultRequest<?> signableRequest = new DefaultRequest<>(service);
+
+        HttpHost host = (HttpHost) context.getAttribute(HTTP_TARGET_HOST);
+        if (host != null) {
+            signableRequest.setEndpoint(URI.create(host.toURI()));
+        }
+        final HttpMethodName httpMethod =
+                HttpMethodName.fromValue(request.getRequestLine().getMethod());
+        signableRequest.setHttpMethod(httpMethod);
+        try {
+            signableRequest.setResourcePath(uriBuilder.build().getRawPath());
+        } catch (URISyntaxException e) {
+            throw new IOException("Invalid URI" , e);
         }
 
-        // 서명 가능한 요청을 생성합니다.
-        SignableRequest<?> signableRequest = new SignableApacheRequest(requestWrapper, URI.create(uri));
+        if (request instanceof HttpEntityEnclosingRequest) {
+            HttpEntityEnclosingRequest httpEntityEnclosingRequest =
+                    (HttpEntityEnclosingRequest) request;
+            if (httpEntityEnclosingRequest.getEntity() == null) {
+                signableRequest.setContent(new ByteArrayInputStream(new byte[0]));
+            } else {
+                signableRequest.setContent(httpEntityEnclosingRequest.getEntity().getContent());
+            }
+        }
+        signableRequest.setParameters(nvpToMapParams(uriBuilder.getQueryParams()));
+        signableRequest.setHeaders(headerArrayToMap(request.getAllHeaders()));
 
-        // 요청에 서명을 적용합니다.
+        // Sign it
         signer.sign(signableRequest, awsCredentialsProvider.getCredentials());
 
-        // 서명된 헤더를 원본 요청에 추가합니다.
-        for (Map.Entry<String, String> header : signableRequest.getHeaders().entrySet()) {
-            request.setHeader(header.getKey(), header.getValue());
+        // Now copy everything back
+        request.setHeaders(mapToHeaderArray(signableRequest.getHeaders()));
+        if (request instanceof HttpEntityEnclosingRequest) {
+            HttpEntityEnclosingRequest httpEntityEnclosingRequest =
+                    (HttpEntityEnclosingRequest) request;
+            if (httpEntityEnclosingRequest.getEntity() != null) {
+                BasicHttpEntity basicHttpEntity = new BasicHttpEntity();
+                basicHttpEntity.setContent(signableRequest.getContent());
+                httpEntityEnclosingRequest.setEntity(basicHttpEntity);
+            }
         }
     }
 
-    private static class SignableApacheRequest implements SignableRequest<HttpRequest> {
-        private final HttpRequestWrapper request;
-        private final URI uri;
-        private final Map<String, String> headers = new HashMap<>();
-        private final Map<String, List<String>> parameters = new HashMap<>();
-        private InputStream content;
-
-        public SignableApacheRequest(HttpRequestWrapper request, URI uri) {
-            this.request = request;
-            this.uri = uri;
-            this.content = null;
+    /**
+     *
+     * @param params list of HTTP query params as NameValuePairs
+     * @return a multimap of HTTP query params
+     */
+    private static Map<String, List<String>> nvpToMapParams(final List<NameValuePair> params) {
+        Map<String, List<String>> parameterMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        for (NameValuePair nvp : params) {
+            List<String> argsList =
+                    parameterMap.computeIfAbsent(nvp.getName(), k -> new ArrayList<>());
+            argsList.add(nvp.getValue());
         }
+        return parameterMap;
+    }
 
-        @Override
-        public void addHeader(String name, String value) {
-            request.addHeader(name, value);
-            headers.put(name, value);
-        }
-
-        @Override
-        public Map<String, String> getHeaders() {
-            Map<String, String> allHeaders = new HashMap<>();
-            for (Header header : request.getAllHeaders()) {
-                allHeaders.put(header.getName(), header.getValue());
-            }
-            return allHeaders;
-        }
-
-        @Override
-        public String getResourcePath() {
-            return uri.getPath();
-        }
-
-        @Override
-        public Map<String, List<String>> getParameters() {
-            // 쿼리 파라미터를 파싱합니다.
-            Map<String, List<String>> params = new HashMap<>();
-            String query = uri.getQuery();
-            if (query != null && !query.isEmpty()) {
-                String[] pairs = query.split("&");
-                for (String pair : pairs) {
-                    int idx = pair.indexOf("=");
-                    String key = idx > 0 ? pair.substring(0, idx) : pair;
-                    String value = idx > 0 && pair.length() > idx + 1 ? pair.substring(idx + 1) : "";
-                    params.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
-                }
-            }
-            return params;
-        }
-
-        @Override
-        public void addParameter(String name, String value) {
-            parameters.computeIfAbsent(name, k -> new ArrayList<>()).add(value);
-        }
-
-        public void setParameters(Map<String, List<String>> parameters) {
-            this.parameters.clear();
-            this.parameters.putAll(parameters);
-        }
-
-        @Override
-        public URI getEndpoint() {
-            return URI.create(uri.getScheme() + "://" + uri.getHost());
-        }
-
-        @Override
-        public HttpMethodName getHttpMethod() {
-            return HttpMethodName.fromValue(request.getMethod());
-        }
-
-        @Override
-        public InputStream getContent() {
-            if (content != null) {
-                return content;
-            } else if (request instanceof HttpEntityEnclosingRequest) {
-                try {
-                    HttpEntity entity = ((HttpEntityEnclosingRequest) request).getEntity();
-                    if (entity == null) {
-                        return new ByteArrayInputStream(new byte[0]);
-                    }
-                    if (!entity.isRepeatable() || entity.getContentLength() < 0) {
-                        entity = new BufferedHttpEntity(entity);
-                        ((HttpEntityEnclosingRequest) request).setEntity(entity);
-                    }
-                    return entity.getContent();
-                } catch (IOException e) {
-                    return new ByteArrayInputStream(new byte[0]);
-                }
-            } else {
-                return new ByteArrayInputStream(new byte[0]);
+    /**
+     * @param headers modeled Header objects
+     * @return a Map of header entries
+     */
+    private static Map<String, String> headerArrayToMap(final Header[] headers) {
+        Map<String, String> headersMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        for (Header header : headers) {
+            if (!skipHeader(header)) {
+                headersMap.put(header.getName(), header.getValue());
             }
         }
+        return headersMap;
+    }
 
-        @Override
-        public InputStream getContentUnwrapped() {
-            return null;
-        }
+    /**
+     * @param header header line to check
+     * @return true if the given header should be excluded when signing
+     */
+    private static boolean skipHeader(final Header header) {
+        return ("content-length".equalsIgnoreCase(header.getName())
+                && "0".equals(header.getValue())) // Strip Content-Length: 0
+                || "host".equalsIgnoreCase(header.getName()); // Host comes from endpoint
+    }
 
-        @Override
-        public void setContent(InputStream content) {
-            this.content = content;
+    /**
+     * @param mapHeaders Map of header entries
+     * @return modeled Header objects
+     */
+    private static Header[] mapToHeaderArray(final Map<String, String> mapHeaders) {
+        Header[] headers = new Header[mapHeaders.size()];
+        int i = 0;
+        for (Map.Entry<String, String> headerEntry : mapHeaders.entrySet()) {
+            headers[i++] = new BasicHeader(headerEntry.getKey(), headerEntry.getValue());
         }
-
-        @Override
-        public ReadLimitInfo getReadLimitInfo() {
-            return () -> 0;
-        }
-
-        @Override
-        public int getTimeOffset() {
-            return 0;
-        }
-
-        @Override
-        public HttpRequest getOriginalRequestObject() {
-            return request;
-        }
+        return headers;
     }
 }
--- services/SearchService/src/main/java/kyobo/cda/SearchService/config/AwsConfig.java
+++ services/SearchService/src/main/java/kyobo/cda/SearchService/config/AwsConfig.java
@@ -1,8 +1,10 @@
 package kyobo.cda.SearchService.config;
 
+import com.amazonaws.auth.AWS4Signer;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import org.apache.http.HttpHost;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.RestHighLevelClient;
@@ -15,23 +17,37 @@ public class AwsConfig {
 
     @Value("${opensearch.host}")
     private String host;
+
     @Value("${opensearch.region}")
     private String region;
-    private static final String serviceName = "es";
+
+    private static final String serviceName = "es"; // Amazon OpenSearch Service는 여전히 "es"를 사용
 
     @Bean
     public RestHighLevelClient restHighLevelClient() {
+        // AWS 자격 증명 제공자 설정
         AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+
+        // AWS4 서명자 설정
+        AWS4Signer signer = new AWS4Signer();
+        signer.setServiceName(serviceName);
+        signer.setRegionName(region);
 
         // AWSRequestSigningApacheInterceptor 생성
         AWSRequestSigningApacheInterceptor interceptor = new AWSRequestSigningApacheInterceptor(
                 serviceName,
-                region,
+                signer,
                 credentialsProvider
         );
 
+        // RestClientBuilder에 인터셉터 추가
         RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(host, 443, "https"))
-                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.addInterceptorLast(interceptor));
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.addInterceptorLast(interceptor);
+                    }
+                });
 
         return new RestHighLevelClient(restClientBuilder);
     }
